353d099fdf178745e7489ab704db75066cf0843e
Yashin <yashinndsantos@gmail.com>
2019-11-09 21:55:06 -0300

    feat: balance

---
+++ src/views/AccountBalance.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props{
+    balance: string;
+};
+
+const AccountBalance = (props: Props) => {
+    return(
+            <div style={accountBalanceStyle}>
+                <AccountBalanceMessage>Account balance:</AccountBalanceMessage>
+                <Balance>{props.balance}</Balance>
+            </div>
+    );
+};
+
+export default AccountBalance
+
+const AccountBalanceMessage = styled.p`
+    font-size: 1rem;
+    color: black
+`
+const Balance = styled.p`
+    font-size: 1rem;
+    color: green
+`
+const accountBalanceStyle = {
+    marginTop: "15px",
+    display: "flex",
+    justifyContent: "space-between",
+    width: "250px"
+    }
