42e09fcb1d42df64698117e4ffe2d094e646bcae
Yashin <yashinndsantos@gmail.com>
2019-11-10 12:18:08 -0300

    chore: withdraw screen

--- src/views/Deposit.tsx
+++ src/views/Deposit.tsx
@@ -1,9 +1,10 @@
 import React, {Component, FormEvent, ChangeEvent} from 'react'
-import { Col, Container, Form, Button } from 'reactstrap'
+import { Container, Form, Button } from 'reactstrap'
 import styled from 'styled-components'
 import Forms from '../components/Forms'
 
 interface State {
+    email: string,
     value: string
 };
 
@@ -14,7 +15,11 @@ interface Props{
 export default class Withdraw extends Component<Props, State> {
     constructor(props: Props){
         super(props);
-        this.state = { value: ""}
+        this.state = {email: "", value: ""}
+    };
+
+    email = (event: ChangeEvent<HTMLInputElement>) =>{
+        this.setState({email: event.target.value})
     };
 
     value = (event: ChangeEvent<HTMLInputElement>) =>{
@@ -27,25 +32,28 @@ export default class Withdraw extends Component<Props, State> {
     }
 
     render(){
-        const { value } = this.state
-        const formOne = [
-            {
-                label: "Value",
-                value: value,
-                onChange: this.value,
-                type: "text"
-            }
-        ];
-
+        const { email, value } = this.state
+        const formOne = [{
+            label: "Email",
+            value: email,
+            onChange: this.email,
+            type: "text"
+        },
+        {
+            label: "Value",
+            value: value,
+            onChange: this.value,
+            type: "text"
+        }]
         return(
             <Container>
                 <HeaderFunctions>
                     <h1>Withdraw </h1>
                 </HeaderFunctions>
                 <Form onSubmit={this.withdraw}>
-                    <Col md="6">
+                    <FormContainer>
                         {Forms({forms: formOne})}
-                    </Col>
+                    </FormContainer>
                     <ButtonContainer>
                         <Button color="success" size="sm">Do!</Button>
                     </ButtonContainer>
@@ -57,7 +65,6 @@ export default class Withdraw extends Component<Props, State> {
 
 const HeaderFunctions = styled.div`
     margin-top: 5px;
-    margin-bottom: 25px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -67,4 +74,11 @@ const ButtonContainer = styled.div`
     display: flex;
     justify-content: center;
     width: 530px ;
+`
+const FormContainer = styled(Container)`
+    margin-top: 25px;
+    width: 600px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
 `
